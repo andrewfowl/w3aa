117f8b9482e4d9668a8e881b86196aa47c3b0124
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-11-11 01:09:22 -0500

    Create events.ts

---
+++ src/data/events.ts
@@ -0,0 +1,18 @@
+export const events = [
+  {
+    id: 'forum-2024',
+    title: 'First Open Forum',
+    date: 'November 5, 2024',
+    time: '9:00 AM PST',
+    location: 'San Francisco, CA',
+    description: 'Inaugural forum addressing pressing challenges in digital asset accounting.'
+  },
+  {
+    id: 'standards-workshop',
+    title: 'Standards Workshop',
+    date: 'November 6, 2024',
+    time: '10:00 AM PST',
+    location: 'Virtual',
+    description: 'Collaborative workshop on developing unified standards for tokenized assets.'
+  }
+];
