e8ef5536bd97cb5b05412460809239e6b7f4bccb
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-11-11 01:08:39 -0500

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Event {
+  id: string;
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  description: string;
+}
+
+export interface Question {
+  name: string;
+  email: string;
+  topic: string;
+  question: string;
+}
