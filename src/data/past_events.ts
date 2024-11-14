934c59f8f8ac82c25005d41bd29cb723f45a243b
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-11-13 20:51:09 -0500

    Create past_events.ts

---
+++ src/data/past_events.ts
@@ -0,0 +1,9 @@
+export const past_events=[{
+    id: 'w3aa_01_11-05-2024',
+    title: 'Meeting #1',
+    date: 'November 5, 2024',
+    time: '1:00 PM PST',
+    location: 'Virtual',
+    description: 'First meeting addressing pressing challenges in digital asset accounting.',
+    URL: 'https://lu.ma/a7k4v2mh'
+  }];
