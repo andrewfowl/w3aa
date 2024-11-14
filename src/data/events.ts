ebde04bd785c52a750990856cb2ab99696c1677d
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-11-13 20:50:50 -0500

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,14 +1,5 @@
 export const events = [
   {
-    id: 'w3aa_01_11-05-2024',
-    title: 'Meeting #1',
-    date: 'November 5, 2024',
-    time: '1:00 PM PST',
-    location: 'Virtual',
-    description: 'First meeting addressing pressing challenges in digital asset accounting.',
-    URL: 'https://lu.ma/a7k4v2mh'
-  },
-  {
     id: 'w3aa_02_11-27-2024',
     title: 'Meeting #2',
     date: 'November 27, 2024',
