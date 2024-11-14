e86e73a1c61cb8916d88c4c911924d15bf013140
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-11-13 20:51:42 -0500

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,6 +8,16 @@ export interface Event {
   URL: string;
 }
 
+export interface PastEvent {
+  id: string;
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  description: string;
+  URL: string;
+}
+
 export interface Question {
   name: string;
   email: string;
