67dd8ae9cb4e5df1d3eda04e3614010a5e09083a
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-11-25 21:26:33 -0500

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -9,29 +9,29 @@ export const events = [
     W3AA hosts virtual meetings of crypto accounting professionals. These small-group events facilitate discussions of issues and 
     challenges that are at the top of crypto finance teams' minds and the agenda is shaped and driven by the participants.
         
-        At our first meeting, we met with James Ting, US Controller at Ripple, and discussed issues related to stablecoin accounting. 
+    At our first meeting, we met with James Ting, US Controller at Ripple, and discussed issues related to stablecoin accounting. 
     We identified that accounting for USDT remains a point of significant argument between some practitioners. Currently, the only known 
     to us ‘official’ stance on this question, is the position taken by Mega Matrix Group in 2023. Mega Matrix Group restated their financial 
     statements to reclassify USDT out of the financial assets category with the following justification:
     
-        ‘As Tether reserves the right under its user agreement to redeem USDT by in-kind redemptions of other assets it holds in its reserves and 
+    <<<As Tether reserves the right under its user agreement to redeem USDT by in-kind redemptions of other assets it holds in its reserves and 
     as Tether has held precious metals and other non-financial assets in its reserves, it does not appear that USDT meets the definition of 
     a financial instrument under ASC 825-10-20. The Company reclassified USDT, amounting $2,300 as of March 31, 2023, from stable coins to 
     digital assets. The reclassification had no impact on net assets as of December 31, 2022, and revenues and net loss for the three months 
-    ended March 31, 2023’
+    ended March 31, 2023>>>
         
-        After our meeting we performed an external consultation with the AICPA Accounting and Auditing Hotline and the staff member expressed 
+    After our meeting we performed an external consultation with the AICPA Accounting and Auditing Hotline and the staff member expressed 
     the view that USDT is appropriate to account for as financial assets. It should be noted that this is not an official position of AICPA, but 
     an individual opinion of a staff who was helping to answer the question.
     
-        We also discussed considerations over accounting for fractional ownership rights on tokenized US treasury T-bills.
+    We also discussed considerations over accounting for fractional ownership rights on tokenized US treasury T-bills.
 
-        About Ripple
-        Using proven crypto and blockchain technology honed over a decade, Ripple’s enterprise-grade solutions are faster, more transparent, and 
+    About Ripple
+    Using proven crypto and blockchain technology honed over a decade, Ripple’s enterprise-grade solutions are faster, more transparent, and 
     more cost-effective than traditional financial services. Our customers use these solutions to source crypto, facilitate instant payments, empower 
     their treasury, engage new audiences, lower capital requirements, and drive new revenue.
         
-        Founded in 2012, Ripple's vision is to enable a world where value moves as seamlessly as information flows today—an Internet of Value. 
+    Founded in 2012, Ripple's vision is to enable a world where value moves as seamlessly as information flows today—an Internet of Value. 
     Ripple is the only enterprise blockchain company today with products in commercial use. Ripple’s global payments network includes over 300 customers
     across 40+ countries and six continents..'
   },
