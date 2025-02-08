fbd63d4360dbc87e816a617cab75c692664925a5
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2025-02-08 08:41:14 -0500

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,32 +1,5 @@
 export const events = [
   {
-    id: 'w3aa_01_11-05-2024',
-    title: 'Meeting #1',
-    date: 'November 5, 2024',
-    time: '1:00 PM PST',
-    location: 'Virtual',
-    description: 'At our first meeting, we met with James Ting, US Controller at Ripple, and discussed issues related to stablecoin accounting. We identified that accounting for USDT remains a point of significant argument between some practitioners. Currently, the only known to us official stance on this question is the position taken by Mega Matrix Group in 2023. Mega Matrix Group restated its financial statements to reclassify USDT out of the financial assets category with the following justification: `As Tether reserves the right under its user agreement to redeem USDT by in-kind redemptions of other assets it holds in its reserves and as Tether has held precious metals and other non-financial assets in its reserves, it does not appear that USDT meets the definition of a financial instrument under ASC 825-10-20.` After the meeting, we performed an external consultation with the AICPA Accounting and Auditing Hotline. The staff member expressed  the view that USDT is appropriate to account for as financial assets. It should be noted that this is not an official position of AICPA, but  an individual opinion of a staff who was helping to answer the question. We also discussed considerations over accounting for fractional ownership rights on tokenized US treasury T-bills.',
-    URL: 'https://lu.ma/q1q3bbct'
-},
-  {
-    id: 'w3aa_02_11-27-2024',
-    title: 'Meeting #2',
-    date: 'November 27, 2024',
-    time: '4:00 PM EST',
-    location: 'Virtual',
-    description: 'Second meeting addressing pressing challenges in digital asset accounting.',
-    URL: 'https://lu.ma/q1q3bbct'
-  },
-  {
-    id: 'w3aa_03_01-29-2025',
-    title: 'Meeting #3',
-    date: 'January 29, 2025',
-    time: '4:00 PM EST',
-    location: 'Virtual',
-    description: 'Third meeting addressing pressing challenges in digital asset accounting.',
-    URL: 'https://lu.ma/p78unbuk'
-  },
-  {
     id: 'w3aa_04_02-26-2025',
     title: 'Meeting #4',
     date: 'February 26, 2025',
